c5e3c7dec6bbacecddac931e63d9a13eedf41da8
Egor Bronnikov <bronnikov.40@mail.ru>
2023-11-27 03:30:54 +0300

    Solve part of `Part 4` tasks

--- src/Part4/Tasks.hs
+++ src/Part4/Tasks.hs
@@ -1,32 +1,47 @@
 module Part4.Tasks where
 
-import Util(notImplementedYet)
+import Util (notImplementedYet)
 
 -- Перевёрнутый связный список -- хранит ссылку не на последующию, а на предыдущую ячейку
 data ReverseList a = REmpty | (ReverseList a) :< a
+
 infixl 5 :<
 
 -- Функция-пример, делает из перевёрнутого списка обычный список
 -- Использовать rlistToList в реализации классов запрещено =)
 rlistToList :: ReverseList a -> [a]
 rlistToList lst =
-    reverse (reversed lst)
-    where reversed REmpty = []
-          reversed (init :< last) = last : reversed init
+  reverse (reversed lst)
+  where
+    reversed REmpty = []
+    reversed (init :< last) = last : reversed init
 
 -- Реализуйте обратное преобразование
 listToRlist :: [a] -> ReverseList a
-listToRlist = notImplementedYet
+listToRlist = listToRlist' REmpty
+  where
+    listToRlist' accumulator [] = accumulator
+    listToRlist' accumulator (x : xs) = listToRlist' (accumulator :< x) xs
 
 -- Реализуйте все представленные ниже классы (см. тесты)
-instance Show (ReverseList a) where
-    showsPrec = notImplementedYet
-    show = notImplementedYet
+instance Show a => Show (ReverseList a) where
+  showsPrec = notImplementedYet
+  show x = "[" <> show' x <> "]"
+    where
+      show' REmpty = ""
+      show' (REmpty :< x) = show x
+      show' (xs :< x) = show' xs <> "," <> show x
+
 instance Eq (ReverseList a) where
-    (==) = notImplementedYet
-    (/=) = notImplementedYet
-instance Semigroup (ReverseList a) where
-instance Monoid (ReverseList a) where
-instance Functor ReverseList where
-instance Applicative ReverseList where
-instance Monad ReverseList where
+  (==) = notImplementedYet
+  (/=) = notImplementedYet
+
+instance Semigroup (ReverseList a)
+
+instance Monoid (ReverseList a)
+
+instance Functor ReverseList
+
+instance Applicative ReverseList
+
+instance Monad ReverseList
