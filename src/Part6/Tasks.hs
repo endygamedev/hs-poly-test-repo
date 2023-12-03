6bc4bf625cf2061710221d4a7158a92a1468cbad
Egor Bronnikov <bronnikov.40@mail.ru>
2023-12-03 16:44:28 +0300

    Solve `Part 6` tasks

--- src/Part6/Tasks.hs
+++ src/Part6/Tasks.hs
@@ -1,38 +1,47 @@
 {-# LANGUAGE FlexibleInstances #-}
+
 module Part6.Tasks where
 
+import Data.List
+import Data.Map hiding (lookup, map)
 import Util (notImplementedYet)
-import Data.Map
 
 -- Разреженное представление матрицы. Все элементы, которых нет в sparseMatrixElements, считаются нулями
-data SparseMatrix a = SparseMatrix {
-                                sparseMatrixWidth :: Int,
-                                sparseMatrixHeight :: Int,
-                                sparseMatrixElements :: Map (Int, Int) a
-                         } deriving (Show, Eq)
+data SparseMatrix a = SparseMatrix
+  { sparseMatrixWidth :: Int,
+    sparseMatrixHeight :: Int,
+    sparseMatrixElements :: Map (Int, Int) a
+  }
+  deriving (Show, Eq)
 
 -- Определите класс типов "Матрица" с необходимыми (как вам кажется) операциями,
 -- которые нужны, чтобы реализовать функции, представленные ниже
 class Matrix mx where
+  zero :: Int -> Int -> mx
+  eye :: Int -> mx
+  multiplyMatrix :: mx -> mx -> mx
 
 -- Определите экземпляры данного класса для:
 --  * числа (считается матрицей 1x1)
 --  * списка списков чисел
 --  * типа SparseMatrix, представленного выше
 instance Matrix Int where
-instance Matrix [[Int]] where
-instance Matrix (SparseMatrix Int) where
+  zero _ _ = 0
+  eye _ = 1
+  multiplyMatrix x y = x * y
 
--- Реализуйте следующие функции
--- Единичная матрица
-eye :: Matrix m => Int -> m
-eye w = notImplementedYet
--- Матрица, заполненная нулями
-zero :: Matrix m => Int -> Int -> m
-zero w h = notImplementedYet
--- Перемножение матриц
-multiplyMatrix :: Matrix m => m -> m -> m
-multiplyMatrix = notImplementedYet
+instance Matrix [[Int]] where
+  zero n m = [[0 | j <- [0 .. (n - 1)]] | i <- [0 .. (m - 1)]]
+  eye n = [[if i == j then 1 else 0 | j <- [0 .. (n - 1)]] | i <- [0 .. (n - 1)]]
+  multiplyMatrix x y = [[sum $ zipWith (*) rowX colY | colY <- transpose y] | rowX <- x]
+
+instance Matrix (SparseMatrix Int) where
+  zero n m = SparseMatrix n m mempty
+  eye n = SparseMatrix n n sparseMatrixElements
+    where
+      sparseMatrixElements = fromList [((i, i), 1) | i <- [0 .. (n - 1)]]
+  multiplyMatrix x y = notImplementedYet
+
 -- Определитель матрицы
 determinant :: Matrix m => m -> Int
 determinant = notImplementedYet
--- src/Part6/Tests.hs
+++ src/Part6/Tests.hs
@@ -1,24 +1,39 @@
 module Part6.Tests where
 
 import qualified Data.Map
-
+import Part6.Tasks
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
-import Part6.Tasks
 
 unit_eye = do
-    eye 1 @?= one
-    eye 1 @?= [[one]]
-    eye 1 @?= SparseMatrix 1 1 (Data.Map.fromList [((0, 0), one)])
-    eye 2 @?= [[one, 0], [0, one]]
-    eye 2 @?= SparseMatrix 2 2 (Data.Map.fromList [((0, 0), one), ((1, 1), one)])
-
-    where one :: Int; one = 1
+  eye 1 @?= one
+  eye 1 @?= [[one]]
+  eye 1 @?= SparseMatrix 1 1 (Data.Map.fromList [((0, 0), one)])
+  eye 2 @?= [[one, 0], [0, one]]
+  eye 2 @?= SparseMatrix 2 2 (Data.Map.fromList [((0, 0), one), ((1, 1), one)])
+  where
+    one :: Int; one = 1
 
 unit_zero = do
-    zero 1 1 @?= zz
-    zero 2 1 @?= [[zz, zz]]
-    zero 2 2 @?= [[zz, zz], [zz, zz]]
-    zero 5 5 @?= SparseMatrix 5 5 (Data.Map.fromList ([]::[((Int, Int), Int)]))
-    where zz :: Int; zz = 0
+  zero 1 1 @?= zz
+  zero 2 1 @?= [[zz, zz]]
+  zero 2 2 @?= [[zz, zz], [zz, zz]]
+  zero 5 5 @?= SparseMatrix 5 5 (Data.Map.fromList ([] :: [((Int, Int), Int)]))
+  where
+    zz :: Int; zz = 0
 
+unit_multiplyMatrix = do
+  multiplyMatrix one one @?= one
+  multiplyMatrix mxFirst mxSecond @?= mxResult
+  where
+    one :: Int
+    one = 1
+
+    mxFirst :: [[Int]]
+    mxFirst = [[2, 6, 4], [1, 9, 7]]
+
+    mxSecond :: [[Int]]
+    mxSecond = [[3, 2], [4, 10], [11, 8]]
+
+    mxResult :: [[Int]]
+    mxResult = [[74, 96], [116, 148]]
